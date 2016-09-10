612c0a6bf919b53aef677c113cb30655668567e0
Renault Steven <renaultsteven@hotmail.com>
2016-09-10 17:31:48 +0000

    initialisation

---
+++ base2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void) {
+  
+  printf("%d\n", 100*(3/5);
+   
+  
+
+  return EXIT_SUCCESS;
+}
---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void) {
+  int x;
+
+  printf("%s", "entrer un nombre");
+  scanf("%d", &x);
+
+  return EXIT_SUCCESS;
+}
